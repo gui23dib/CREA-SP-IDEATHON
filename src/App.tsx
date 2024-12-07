840011ee59d8c2e531684ba18498878cea396859
João Paulo Tavares da Silva <jpcovinhas0@gmail.com>
2024-12-07 09:49:49 -0300

    fix: altera o layout

--- src/App.tsx
+++ src/App.tsx
@@ -52,11 +52,11 @@ function App() {
 
           <div className="flex flex-col sm:flex-row w-full justify-center">
             <div
-              className="flex flex-col gap-5 w-full items-center justify-center p-8 sm:p-14"
+              className="flex flex-col gap-5 w-full items-center justify-center p-8 sm:p-14 hover:cursor-pointer"
               onClick={() => enterpriseProfileRef.current?.scrollIntoView({ behavior: "smooth" })}
             >
               <Building className="w-24 h-24 sm:w-32 sm:h-32" />
-              <h3 className="text-center text-3xl sm:text-4xl font-bold">Instituicao</h3>
+              <h3 className="text-center text-3xl sm:text-4xl font-bold">Instituição</h3>
             </div>
             <Separator orientation="horizontal" className="bg-black h-1 sm:hidden" />
             <Separator orientation="vertical" className="bg-black w-1 hidden sm:block" />
@@ -127,15 +127,45 @@ function App() {
                         </Button>
                       </div>
                       <div className="flex items-center justify-center w-full sm:w-1/2">
-                        {/* Adicione a imagem aqui */}
+                        <img src="carouselEmpresa.jpeg" alt="mockuplvsl" className=" mt-4 bg-transparent object-cover h-96 w-96 rounded-full" />
                       </div>
                     </div>
                   </CarouselItem>
-                  <CarouselItem>
-                    ...
+                  <CarouselItem className="flex flex-col gap-5 w-full items-center justify-center text-left p-4 sm:p-6">
+                    <div className="flex flex-col sm:flex-row gap-5 w-full items-center justify-center text-left">
+                      <div className="flex items-center justify-center w-full sm:w-1/2">
+                        <img src="carouselLicitacao.jpeg" alt="mockuplvsl" className=" mt-4 bg-transparent object-cover h-96 w-96 rounded-full" />
+                      </div>
+                      <div className="flex flex-col gap-5 w-full sm:w-1/2">
+                        <h3 className="text-justify text-xl sm:text-2xl font-bold">
+                          O Selo de Verificação CREA é a solução ideal para garantir confiança e transparência em seus projetos!
+                        </h3>
+                        <p className="text-justify text-sm sm:text-md">
+                          Ele destaca empresas e profissionais alinhados com as normas regulamentares, facilitando processos e transmitindo segurança para a sociedade. Inclua o selo em seus editais e contratações públicas e assegure que seus projetos sejam conduzidos por quem realmente entende do assunto.
+                        </p>
+                        <Button className="bg-black text-white w-fit">
+                          APLICAR
+                        </Button>
+                      </div>
+                    </div>
                   </CarouselItem>
-                  <CarouselItem>
-                    ...
+                  <CarouselItem className="flex flex-col gap-5 w-full items-center justify-center text-left p-4 sm:p-6">
+                    <div className="flex flex-col sm:flex-row gap-5 w-full items-center justify-center text-left">
+                      <div className="flex flex-col gap-5 w-full sm:w-1/2">
+                        <h3 className="text-justify text-xl sm:text-2xl font-bold">
+                          O Selo de Verificação CREA é a marca que diferencia sua instituição!
+                        </h3>
+                        <p className="text-justify text-sm sm:text-md">
+                          Com ele, sua universidade ou faculdade demonstra, de forma clara e confiável, que está alinhada com as melhores práticas e normas do setor, reforçando seu compromisso com a formação de profissionais preparados para os desafios do mercado. Mostre ao mundo que sua instituição está à frente, investindo em credibilidade e inovação para atrair os melhores talentos!
+                        </p>
+                        <Button className="bg-black text-white w-fit">
+                          APLICAR
+                        </Button>
+                      </div>
+                      <div className="flex items-center justify-center w-full sm:w-1/2">
+                        <img src="carouselPrefeitura.jpeg" alt="mockuplvsl" className=" mt-4 bg-transparent object-cover h-96 w-96 rounded-full" />
+                      </div>
+                    </div>
                   </CarouselItem>
                 </CarouselContent>
                 <CarouselPrevious />
@@ -153,10 +183,12 @@ function App() {
             </p>
           </div>
 
-          <div className="flex flex-col w-full mt-10">
+          <div className="flex flex-col w-full mt-10 gap-5">
             <h2 className="text-center text-4xl sm:text-5xl font-bold">Seja visto primeiro!</h2>
 
-            <img src="" alt="mockuplvsl" className="w-full mt-4" />
+            <div className="flex items-center justify-center w-full bg-transparent h-96">
+              <img src="mock.png" alt="mockuplvsl" className="w-full mt-4 bg-transparent object-contain h-96" />
+            </div>
           </div>
 
           <div className="flex flex-col w-full mt-10 gap-6">
@@ -169,20 +201,20 @@ function App() {
             </h2>
           </div>
         </div>
-        <div className="flex flex-col sm:flex-row w-full px-4 sm:px-10 pb-10">
-          <div className="flex flex-col items-center justify-center w-full h-full p-4 sm:p-5 gap-5 mb-4 sm:mb-0 sm:mr-4 border-solid border-4 border-gray-300 rounded-xl">
+        <div className="flex flex-col sm:flex-row w-full px-4 h-fit sm:px-10 pb-10">
+          <div className="flex flex-col items-center justify-center w-full h-96 p-4 sm:p-5 gap-5 mb-4 sm:mb-0 sm:mr-4 border-solid border-4 border-gray-300 rounded-xl">
             <h3 className="text-2xl sm:text-3xl font-semibold">Estudante</h3>
             <p className="text-center">
               Cadastre seu perfil logo cedo! participando de nossas iniciativas como CREA CAPACITA e CREA JOVEM, pontue e garanta visibilidade para sua primeira vaga na área
             </p>
           </div>
-          <div className="flex flex-col items-center justify-center w-full h-full p-4 sm:p-5 gap-5 mb-4 sm:mb-0 sm:mx-4 border-solid border-4 border-gray-300 rounded-xl">
+          <div className="flex flex-col items-center justify-center w-full h-96 p-4 sm:p-5 gap-5 mb-4 sm:mb-0 sm:mx-4 border-solid border-4 border-gray-300 rounded-xl">
             <h3 className="text-2xl sm:text-3xl font-semibold">Profissional registrado</h3>
             <p className="text-center">
               Garanta pontos e descubra novas oportunidades por meio de nosso banco de talentos, se mantenha engajado para não perder seu ranking! Participe de nossos eventos, ideathons e palestras.
             </p>
           </div>
-          <div className="flex flex-col items-center justify-center w-full h-full p-4 sm:p-5 gap-5 sm:ml-4 border-solid border-4 border-gray-300 rounded-xl">
+          <div className="flex flex-col items-center justify-center w-full h-96 p-4 sm:p-5 gap-5 sm:ml-4 border-solid border-4 border-gray-300 rounded-xl">
             <h3 className="text-2xl sm:text-3xl font-semibold">Profissional não registrado</h3>
             <p className="text-center">
               Tá esperando o que! venha fazer parte do maior Conselho de fiscalização de exercício profissional da América Latina, aproveite de vantagens como:
